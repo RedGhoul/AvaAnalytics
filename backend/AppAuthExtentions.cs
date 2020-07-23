ec29ab85528b33abae3dcf0e277284760daecc70
RedGhoul <avaneesab5@gmail.com>
2020-07-23 14:48:00 -0400

    Auto Razor pages-Better Nav Bar-loginpage-roles

---
+++ backend/AppAuthExtentions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using SharpCounter.Data;
+using SharpCounter.Enities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpCounter
+{
+    public static class AppAuthExtentions
+    {
+        public static async Task CreateAdminRoleForDefaultUser(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            var RoleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var content = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            IdentityResult roleResult;
+
+            //Adding Admin Role
+            var roleCheck = await RoleManager.RoleExistsAsync("Admin");
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            //Assign Admin role to the main User here we have given our newly registered 
+            //login id for Admin management
+            ApplicationUser user = await UserManager.FindByEmailAsync("avaneesab5@gmail.com");
+            if (user != null)
+            {
+                var currentUserRoles = await UserManager.GetRolesAsync(user);
+                if (!currentUserRoles.Contains("Admin"))
+                {
+                    await UserManager.AddToRoleAsync(user, "Admin");
+                }
+
+            }
+        }
+    }
+}
