c4586cdd6a12ad5dd27393340c653fed0e0ea01d
RedGhoul <avaneesab5@gmail.com>
2020-11-10 08:23:28 -0500

    updated hangfire config

--- Presentation/PresentationConfig.cs
+++ Presentation/PresentationConfig.cs
@@ -19,7 +19,7 @@ namespace Presentation
                 SchedulePollingInterval = TimeSpan.FromMinutes(1),
                 HeartbeatInterval = TimeSpan.FromSeconds(20),
                 ServerCheckInterval = TimeSpan.FromSeconds(20),
-                WorkerCount = Environment.ProcessorCount * 2,
+                WorkerCount = Environment.ProcessorCount,
                 ServerName = "Jobs"
             });
             app.UseHangfireDashboard("/hangfire", new DashboardOptions
