186b700b1433394e89878878ed546f313b831e88
RedGhoul <avaneesab5@gmail.com>
2020-11-10 21:21:46 -0500

    update hang fire jobs to excute every min

--- Presentation/HangFire/HangFireJobScheduler.cs
+++ Presentation/HangFire/HangFireJobScheduler.cs
@@ -11,32 +11,32 @@ namespace Presentation.HangFire
             RecurringJob.RemoveIfExists(nameof(BrowserStatsJob));
             RecurringJob.AddOrUpdate<BrowserStatsJob>(nameof(BrowserStatsJob),
                 job => job.Run(JobCancellationToken.Null),
-                "*/30 * * * *", TimeZoneInfo.Local);
+                "* * * * *", TimeZoneInfo.Local);
 
             RecurringJob.RemoveIfExists(nameof(SystemStatsJob));
             RecurringJob.AddOrUpdate<SystemStatsJob>(nameof(SystemStatsJob),
                 job => job.Run(JobCancellationToken.Null),
-                "*/30 * * * *", TimeZoneInfo.Local);
+                "* * * * *", TimeZoneInfo.Local);
 
             RecurringJob.RemoveIfExists(nameof(InteractionByPathStatsJob));
             RecurringJob.AddOrUpdate<InteractionByPathStatsJob>(nameof(InteractionByPathStatsJob),
                 job => job.Run(JobCancellationToken.Null),
-                "*/30 * * * *", TimeZoneInfo.Local);
+                "* * * * *", TimeZoneInfo.Local);
 
             RecurringJob.RemoveIfExists(nameof(LocationStatsJob));
             RecurringJob.AddOrUpdate<LocationStatsJob>(nameof(LocationStatsJob),
                 job => job.Run(JobCancellationToken.Null),
-                "*/30 * * * *", TimeZoneInfo.Local);
+                "* * * * *", TimeZoneInfo.Local);
 
             RecurringJob.RemoveIfExists(nameof(ScreenSizeStatsJob));
             RecurringJob.AddOrUpdate<ScreenSizeStatsJob>(nameof(ScreenSizeStatsJob),
                 job => job.Run(JobCancellationToken.Null),
-                "*/30 * * * *", TimeZoneInfo.Local);
+                "* * * * *", TimeZoneInfo.Local);
 
             RecurringJob.RemoveIfExists(nameof(PageViewStatsJob));
             RecurringJob.AddOrUpdate<PageViewStatsJob>(nameof(PageViewStatsJob),
                 job => job.Run(JobCancellationToken.Null),
-                "*/30 * * * *", TimeZoneInfo.Local);
+                "* * * * *", TimeZoneInfo.Local);
         }
     }
 }
