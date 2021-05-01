ec2c93acde28ab7ba297cc1e5b92f58f57d3e340
RedGhoul <avaneesab5@gmail.com>
2021-04-30 22:16:33 -0400

    Added all old jobs again

--- Presentation/HangFire/HangFireAuthorizationFilter.cs
+++ Presentation/HangFire/HangFireAuthorizationFilter.cs
@@ -9,6 +9,7 @@ namespace Presentation.HangFire
         public bool Authorize(DashboardContext context)
         {
             HttpContext httpContext = context.GetHttpContext();
+            if (httpContext == null) return false;
             string userRole = httpContext.User.FindFirst(ClaimTypes.Role)?.Value;
             if (userRole == null) return false;
             return userRole.Equals("Admin");
--- Presentation/HangFire/HangFireJobScheduler.cs
+++ Presentation/HangFire/HangFireJobScheduler.cs
@@ -7,11 +7,35 @@ namespace Presentation.HangFire
     {
         public static void ScheduleRecurringJobs()
         {
+            RecurringJob.RemoveIfExists(nameof(BrowserStatsJob));
+            RecurringJob.AddOrUpdate<BrowserStatsJob>(nameof(BrowserStatsJob),
+                job => job.Run(JobCancellationToken.Null),
+                "*/5 * * * *", TimeZoneInfo.Local);
 
-            //RecurringJob.RemoveIfExists(nameof(StatsCollectorJob));
-            //RecurringJob.AddOrUpdate<StatsCollectorJob>(nameof(StatsCollectorJob),
-            //    job => job.Run(JobCancellationToken.Null),
-            //    "*/5 * * * *", TimeZoneInfo.Local);
+            RecurringJob.RemoveIfExists(nameof(InteractionByPathStatsJob));
+            RecurringJob.AddOrUpdate<InteractionByPathStatsJob>(nameof(InteractionByPathStatsJob),
+                job => job.Run(JobCancellationToken.Null),
+                "*/5 * * * *", TimeZoneInfo.Local);
+
+            RecurringJob.RemoveIfExists(nameof(LocationStatsJob));
+            RecurringJob.AddOrUpdate<LocationStatsJob>(nameof(LocationStatsJob),
+                job => job.Run(JobCancellationToken.Null),
+                "*/5 * * * *", TimeZoneInfo.Local);
+
+            RecurringJob.RemoveIfExists(nameof(PageViewStatsJob));
+            RecurringJob.AddOrUpdate<PageViewStatsJob>(nameof(PageViewStatsJob),
+                job => job.Run(JobCancellationToken.Null),
+                "*/5 * * * *", TimeZoneInfo.Local);
+
+            RecurringJob.RemoveIfExists(nameof(ScreenSizeStatsJob));
+            RecurringJob.AddOrUpdate<ScreenSizeStatsJob>(nameof(ScreenSizeStatsJob),
+                job => job.Run(JobCancellationToken.Null),
+                "*/5 * * * *", TimeZoneInfo.Local);
+
+            RecurringJob.RemoveIfExists(nameof(SystemStatsJob));
+            RecurringJob.AddOrUpdate<SystemStatsJob>(nameof(SystemStatsJob),
+                job => job.Run(JobCancellationToken.Null),
+                "*/5 * * * *", TimeZoneInfo.Local);
 
         }
     }
